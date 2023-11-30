de4b530813112d47c68bf119e95bc4d3c9cdadec
Lucci Fagela <145944229+laljfagela@users.noreply.github.com>
2023-11-30 22:41:13 +0800

    Update scene1_legends.py

--- scene1_legends.py
+++ scene1_legends.py
@@ -7,7 +7,7 @@ legend_items = {
 }
 
 def display_legend():
-    for row in generate_island_map:
+    for row in island_map:
         print("\nMap Legends:".join(row))
     for item, symbol in legend_items.items():
         print(f"{item}: {symbol}")
